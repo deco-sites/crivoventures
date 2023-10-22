1dfbc03842d8534dd208abdbe28f014e0557004e
Ana Carolina D. C <76822093+devartes@users.noreply.github.com>
2023-10-22 13:20:06 -0300

    feat: add OurPortfolio

---
+++ sections/Main/OurPortfolio.tsx
@@ -0,0 +1,32 @@
+import type { Image as LiveImage } from "deco-sites/std/components/types.ts";
+import Image from "deco-sites/std/components/Image.tsx";
+
+export interface Props {
+    title?: string;
+    logo?: Logo[];
+}
+
+export interface Logo {
+    image: LiveImage;
+    width: number;
+    height?: number;
+    label?: string;
+    url?: string;
+}
+
+export default function OurPortfolio(props: Props) {
+    const { title, logo } = props;
+
+    return (
+        <div class="max-w-[1200px] mx-auto py-[6.6vmax] relative md:top-0 top-[145.721px] px-[20px]">
+            <h3 class="text-[#d1734c] md:text-[33px] text-[24.4909px] font-bold mb-[19.61px] pl-[20px]">{title}</h3>
+            <div class="grid md:grid-cols-4 grid-cols-2 items-center">
+            {logo?.map((item) => (
+                <a href={item.url} class="md:h-full h-[131.948px] flex items-center">
+                    <figure><Image src={item?.image} class="mx-auto" alt={item.label} width={item.width} height={item.height} /></figure>
+                </a>
+            ))}
+            </div>
+        </div>
+    );
+}
