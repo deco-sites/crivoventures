6492e1efa33ad33492e3894ebc6bc8091a608c73
Ana Carolina D. C <76822093+devartes@users.noreply.github.com>
2023-10-22 09:53:53 -0300

    refactor(Main.tsx): fix styles

--- sections/Main/Main.tsx
+++ sections/Main/Main.tsx
@@ -11,8 +11,8 @@ export interface Props {
 export default function Main(props: Props) {
   const { title, image, alt } = props;
   return (
-    <div class="max-w-[1200px] mx-auto flex md:flex-row flex-col md:justify-between items-center py-[3.3vmax] md:top-[101.766px] top-[145.721px] relative z-0 md:px-[40px]">
-      <h3 class="text-[#174b28] md:text-[33px] text-[25.8327px] font-bold md:max-w-[342.15px] md:px-0 px-[17.13%]">
+    <div class="max-w-[1200px] mx-auto flex md:flex-row flex-col md:justify-between items-center pb-[3.3vmax] lg:top-[101.766px] top-[145.721px] relative z-0 md:pl-[40px]">
+      <h3 class="text-[#174b28] md:text-[33px] text-[25.8327px] font-bold md:max-w-[342.15px] md:px-0 px-[17.13%] md:leading-[43.9824px] leading-[26.4331px]">
         {title}
       </h3>
       <figure>
