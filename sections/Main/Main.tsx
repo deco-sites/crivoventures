1f8d9fc205a48aa8d800c0a1493498bab3367f95
Ana Carolina D. C <76822093+devartes@users.noreply.github.com>
2023-10-23 08:31:25 -0300

    refactor: fix styles

--- sections/Main/Main.tsx
+++ sections/Main/Main.tsx
@@ -11,7 +11,7 @@ export interface Props {
 export default function Main(props: Props) {
   const { title, image, alt } = props;
   return (
-    <div class="max-w-[1200px] mx-auto flex md:flex-row flex-col md:justify-between items-center pb-[3.3vmax] lg:top-[101.766px] top-[145.721px] relative z-0 md:pl-[40px]">
+    <div class="max-w-[1200px] mx-auto flex md:flex-row flex-col md:justify-between items-center pb-[3.3vmax] lg:pt-[101.766px] pt-[145.721px] z-0 md:pl-[40px]">
       <h3 class="text-[#174b28] md:text-[33px] text-[25.8327px] font-bold md:max-w-[342.15px] md:px-0 px-[17.13%] md:leading-[43.9824px] leading-[26.4331px]">
         {title}
       </h3>
--- sections/Main/OurPortfolio.tsx
+++ sections/Main/OurPortfolio.tsx
@@ -18,9 +18,9 @@ export default function OurPortfolio(props: Props) {
     const { title, logo } = props;
 
     return (
-        <div class="max-w-[1200px] mx-auto py-[6.6vmax] relative md:top-0 top-[145.721px] px-[20px]">
-            <h3 class="text-[#d1734c] md:text-[33px] text-[24.4909px] font-bold mb-[19.61px] pl-[20px]">{title}</h3>
-            <div class="grid md:grid-cols-4 grid-cols-2 items-center">
+        <div class="max-w-[1200px] mx-auto py-[6.6vmax] relative lg:pt-[101.766px] pt-[145.721px] px-[20px]">
+            <h3 class="text-[#d1734c] md:text-[33px] text-[24.4909px] font-bold mb-[39.61px] pl-[20px]">{title}</h3>
+            <div class="grid md:grid-cols-4 grid-cols-2 items-center md:gap-y-[40px] md:gap-x-[100px] md:pl-[20px]">
             {logo?.map((item) => (
                 <a href={item.url} class="md:h-full h-[131.948px] flex items-center">
                     <figure><Image src={item?.image} class="mx-auto" alt={item.label} width={item.width} height={item.height} /></figure>
