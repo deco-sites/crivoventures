2364d5006f6cd9cef7620c80297bfbd66185a1fb
Ana Carolina D. C <76822093+devartes@users.noreply.github.com>
2023-10-29 19:30:19 -0300

    refactor(About.tsx): fix styles

--- sections/Main/About.tsx
+++ sections/Main/About.tsx
@@ -11,7 +11,7 @@ export interface Props {
     label?: string;
   };
   image: {
-    src: LiveImage;
+    src?: LiveImage;
     width: number;
     height: number;
     label?: string;
@@ -30,7 +30,7 @@ export default function About(props: Props) {
     <div class="max-w-[1098.69px] mx-auto flex flex-col pt-[94.73px]">
       <div class="flex flex-col lg:mt-[50px] md:mt-[105px] mt-[30px]">
         <div class="flex md:flex-row flex-col md:justify-between pb-[50px] md:gap-x-[50px]">
-          <div class="flex flex-col md:w-1/2 lg:px-0 md:pl-[50px] px-[20px] justify-between">
+          <div class="flex flex-col md:w-1/2 lg:px-0 md:pl-[50px] px-[20px] justify-around">
             <figure>
               <Image
                 src={logo.src}
@@ -45,14 +45,20 @@ export default function About(props: Props) {
               class="md:text-[18px] text-[15.7691px] md:pt-[30px] pt-[40px] md:pb-0 pb-[20px] leading-[1.8em] underline-offset-[0.2em] gap-[15px] flex flex-col"
             />
           </div>
-          <figure class="md:w-1/2 md:mb-0 lg:px-0 md:pr-[50px] px-0">
-            <Image
-              src={image.src}
-              class="object-cover w-full h-full"
-              width={image.width}
-              height={image.height}
-              alt={image.label}
-            />
+          <figure
+            class={`md:w-1/2 md:mb-0 lg:px-0 md:pr-[50px] px-0 ${
+              !image?.src && "lg:h-[578px]"
+            }`}
+          >
+            {image?.src && (
+              <Image
+                src={image?.src || ""}
+                class="object-cover w-full h-full"
+                width={image.width}
+                height={image.height}
+                alt={image.label}
+              />
+            )}
           </figure>
         </div>
         <hr class="bg-[#174b28] h-[4px] my-[9px] lg:mx-0 md:mx-[50px] mx-[20px]" />
