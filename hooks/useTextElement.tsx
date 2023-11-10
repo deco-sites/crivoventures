7ecf6ed0033f3d440b4bac1a99f516638277047f
Ana Carolina D. C <76822093+devartes@users.noreply.github.com>
2023-11-10 08:03:56 -0300

    fix: correcting hook logic

--- hooks/useTextElement.tsx
+++ hooks/useTextElement.tsx
@@ -16,6 +16,7 @@ function useTextElementOpacity() {
     const elements = document.querySelectorAll(
       "h1, h2, h3, h4, h5, a:not(:has(figure)) , a > figure , img ,  hr , .cookie , details > summary > p , details > div > p , div > p",
     );
+
     for (const element of elements) {
       const rect = element.getBoundingClientRect();
       const isElementVisible = rect.top >= 0 &&
@@ -26,11 +27,28 @@ function useTextElementOpacity() {
           element.style.transition = "opacity 0.9s 0.261818s ease";
           element.classList.add("opacity-100");
         }
-        if (element.matches("details > div > p") && element.closest("details[open]")) {
-          element.style.transition = "opacity 0.9s 0.261818s ease";
-          element.classList.add("opacity-100");
-        }
       }
+    }
+  };
+
+  const handleDetailsToggle = (event: Event) => {
+    const details = event.target as HTMLElement;
+
+    if (details && details instanceof HTMLDetailsElement) {
+      const paragraphs = details.querySelectorAll("div > p");
+
+      paragraphs.forEach((p) => {
+        if (p instanceof HTMLElement) {
+          p.style.transition = "opacity 0.9s 0.261818s ease";
+          if (details.open) {
+            p.classList.add("opacity-100");
+          } else {
+            p.classList.remove("opacity-100");
+          }
+        }
+      });
+
+      details.removeEventListener("toggle", handleDetailsToggle);
     }
   };
 
@@ -40,8 +58,17 @@ function useTextElementOpacity() {
       checkVisibility();
       addEventListener("scroll", checkVisibility);
     }, 1000);
+
+    const detailsElements = document.querySelectorAll("details");
+    detailsElements.forEach((details) => {
+      details.addEventListener("toggle", handleDetailsToggle);
+    });
+
     return () => {
       removeEventListener("scroll", checkVisibility);
+      detailsElements.forEach((details) => {
+        details.removeEventListener("toggle", handleDetailsToggle);
+      });
     };
   }, []);
 }
